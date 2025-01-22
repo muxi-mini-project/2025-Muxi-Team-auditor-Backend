969adad1ac10a0307244e9b65de27cfa2d2d90b8
cqhasy <3181940782@qq.com>
2025-01-22 18:49:56 +0800

    feat(auth):login and registe

--- service/Auth.go
+++ service/Auth.go
@@ -2,29 +2,50 @@ package service
 
 import (
 	"context"
-	"gorm.io/gorm"
 	"muxi_auditor/api/errors"
+	"muxi_auditor/pkg/jwt"
 	"muxi_auditor/repository/dao"
 	"muxi_auditor/repository/model"
 )
 
-func NewAuthService(userDAO *dao.UserDAO) *AuthService {
-	return &AuthService{userDAO: userDAO}
+func NewAuthService(userDAO *dao.UserDAO, redisJwtHandler *jwt.RedisJWTHandler) *AuthService {
+	return &AuthService{userDAO: userDAO, redisJwtHandler: redisJwtHandler}
 }
 
 type AuthService struct {
-	userDAO *dao.UserDAO
+	userDAO         *dao.UserDAO
+	redisJwtHandler *jwt.RedisJWTHandler
 }
 
-func (s *AuthService) Login(ctx context.Context, username string) error {
+func (s *AuthService) Login(ctx context.Context, email string) (string, string, error) {
 	//随便写的逻辑,需要修改
-	err := s.userDAO.Create(ctx, &model.User{
-		Name:  username,
-		Model: gorm.Model{},
-	})
+	user, err := s.userDAO.FindByEmail(ctx, email)
 	if err != nil {
-		return errors.LOGIN_ERROR(err)
+		return "", "", err
 	}
+	if user == nil {
+		return "0", "", nil
+	}
+	token, err := s.redisJwtHandler.Jwt.SetJWTToken(user.ID, user.Name)
+	if err != nil {
+		return "", "", err
+	}
+	return user.Name, token, nil
 	//执行注册的具体逻辑
-	return nil
+
+}
+func (s *AuthService) Register(ctx context.Context, email string, username string) (string, error) {
+	user := model.User{
+		Email: email,
+		Name:  username,
+	}
+	err := s.userDAO.Create(ctx, &user)
+	if err != nil {
+		return "", errors.LOGIN_ERROR(err)
+	}
+	token, err := s.redisJwtHandler.Jwt.SetJWTToken(user.ID, user.Name)
+	if err != nil {
+		return "", err
+	}
+	return token, nil
 }
