e4c828df6828cd70388c10752b33c0ae66e403d3
sunjunnan <2101763682@qq.com>
2025-01-24 21:47:24 +0800

    fix:修复中间件相关错误

--- muxishenhe/token/token.go
+++
@@ -1,8 +0,0 @@
-package token
-
-type Token struct {
-	AccessToken    string `json:"access_token"`
-	AccessExpired  int    `json:"access_expired"`
-	RefreshToken   string `json:"refresh_token"`
-	RefreshExpired int    `json:"refresh_expired"`
-}
