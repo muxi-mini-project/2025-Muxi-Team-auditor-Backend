8236b203cf8abf4e43be82ebd7b5a716dcb213be
cqhasy <3181940782@qq.com>
2025-02-24 22:26:38 +0800

    (feat)完善功能优化dao层

--- service/Auth.go
+++ service/Auth.go
@@ -4,36 +4,41 @@ import (
 	"context"
 	merr "errors"
 	"github.com/gin-gonic/gin"
+	"github.com/qiniu/go-sdk/v7/storagev2/credentials"
+	"github.com/qiniu/go-sdk/v7/storagev2/uptoken"
 	"muxi_auditor/api/errors"
 	"muxi_auditor/api/request"
+	"muxi_auditor/config"
 	"muxi_auditor/pkg/jwt"
 	"muxi_auditor/repository/dao"
 	"muxi_auditor/repository/model"
+	"time"
 )
 
-func NewAuthService(userDAO *dao.UserDAO, redisJwtHandler *jwt.RedisJWTHandler) *AuthService {
-	return &AuthService{userDAO: userDAO, redisJwtHandler: redisJwtHandler}
+func NewAuthService(userDAO *dao.UserDAO, redisJwtHandler *jwt.RedisJWTHandler, conf *config.QiNiuYunConfig) *AuthService {
+	return &AuthService{userDAO: userDAO, redisJwtHandler: redisJwtHandler, conf: conf}
 }
 
 type AuthService struct {
 	userDAO         *dao.UserDAO
 	redisJwtHandler *jwt.RedisJWTHandler
+	conf            *config.QiNiuYunConfig
 }
 
-func (s *AuthService) Login(ctx context.Context, email string) (string, string, error) {
+func (s *AuthService) Login(ctx context.Context, email string) (string, string, int, error) {
 	//随便写的逻辑,需要修改
 	user, err := s.userDAO.FindByEmail(ctx, email)
 	if err != nil {
-		return "", "", err
+		return "", "", 0, err
 	}
 	if user == nil {
-		return "0", "", nil
+		return "", "", 0, nil
 	}
 	token, err := s.redisJwtHandler.Jwt.SetJWTToken(user.ID, user.Name, user.UserRole)
 	if err != nil {
-		return "", "", err
+		return "", "", 0, err
 	}
-	return user.Name, token, nil
+	return user.Name, token, user.UserRole, nil
 	//执行注册的具体逻辑
 
 }
@@ -41,7 +46,7 @@ func (s *AuthService) Register(ctx context.Context, email string, username strin
 	user := model.User{
 		Email:    email,
 		Name:     username,
-		UserRole: 1,
+		UserRole: 0,
 	}
 	err := s.userDAO.Create(ctx, &user)
 	if err != nil {
@@ -65,7 +70,6 @@ func (s *AuthService) UpdateMyInfo(ctx context.Context, req request.UpdateUserRe
 	if req.Email == "" || req.Name == "" {
 		return merr.New("email and name are required")
 	}
-
 	// Check if user exists
 	existingUser, err := s.userDAO.Read(ctx, id)
 	if err != nil {
@@ -77,9 +81,25 @@ func (s *AuthService) UpdateMyInfo(ctx context.Context, req request.UpdateUserRe
 	var user model.User
 	user.Email = req.Email
 	user.Name = req.Name
-	err = s.userDAO.Update(ctx, &user)
+	user.Avatar = req.Avatar
+	err = s.userDAO.Update(ctx, &user, id)
 	if err != nil {
 		return err
 	}
 	return nil
+}
+func (s *AuthService) GetQiToken(ctx context.Context) (string, error) {
+	accesskey := s.conf.AccessKey
+	secretkey := s.conf.SecretKey
+	bucket := s.conf.Bucket
+	mac := credentials.NewCredentials(accesskey, secretkey)
+	putPolicy, err := uptoken.NewPutPolicy(bucket, time.Now().Add(1*time.Hour))
+	if err != nil {
+		return "", err
+	}
+	upToken, err := uptoken.NewSigner(putPolicy, mac).GetUpToken(context.Background())
+	if err != nil {
+		return upToken, err
+	}
+	return upToken, nil
 }
